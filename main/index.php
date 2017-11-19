ab33aea74a3708b6a413c2f4184359b3b7915627
lifeinbofyx@sina.com <lifeinbofyx@sina.com>
2017-11-19 18:40:57 +0800

    first commit
    first commit.

---
+++ main/index.php
@@ -0,0 +1,226 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>
+<html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>
+<html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>
+<html class="no-js lt-ie9"> <![endif]-->
+<html class='no-js'>
+<!-- Added by HTTrack -->
+<meta http-equiv="content-type" content="text/html;charset=utf-8"/>
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=9; IE=8; IE=EDGE" />
+  <meta content='width=device-width,initial-scale=1, user-scalable=no' name='viewport'>
+  <title>【OPPO智能手机官网】OPPO R11s 全面屏拍照手机_前后2000万，拍照更清晰 </title>
+  <meta name="keywords" content="OPPO手机,OPPO智能手机大全,OPPO智能手机最新报价,OPPO智能手机图片,OPPO智能手机机型,OPPOR11s,OPPO全面屏手机 " />
+  <meta name="description" content="OPPO 更多年轻人选择的拍照手机。国产手机越来越受青睐，在中国，更多年轻人选择OPPO全面屏拍照手机。十年来，OPPO一直专注手机拍照的技术创新，如今，全球超过2亿年轻人正在使用OPPO拍照手机。据权威数据机构IDC统计，OPPO 2016年已成为中国手机市场出货量第一品牌。 " />
+  <meta property="qc:admins" content="1257363774670076375" />
+  <meta name="baidu-site-verification" content="oOoWSh1xnd" />
+
+  	<link media="all" type="text/css" rel="stylesheet" href="https://shopfs.myoppo.com/cn/assets/stylesheets/styles.css?v=201710091028">
+
+
+  <!-- <script src="http://www.oppo.com/cn/lib/modernizr.custom.js"></script>
+ --> 
+
+  	<script type="text/javascript">
+    	function checkBrowser(){
+			var browser = {};
+			var div = document.createElement( "div" );
+			div.innerHTML = "<link/><table></table><a href='/a'>a</a><input type='checkbox'/>";
+			var a = div.getElementsByTagName( "a" )[0];
+			style = a && a.style;
+			if (!style) {
+				return;
+			}
+			style.cssText = "float:left;opacity:.5";
+			// Support: IE<9
+			// Make sure that element opacity exists (as opposed to filter)
+			browser.opacity = style.opacity === "0.5";
+
+			// Verify style float existence
+			// (IE uses styleFloat instead of cssFloat)
+			browser.cssFloat = !!style.cssFloat;
+			if(browser.opacity && browser.cssFloat){
+				return true;
+			}else{
+				//IE 8测试
+        var userAgent = navigator.userAgent.toLocaleUpperCase();
+        var msie = /MSIE [\d\.]+/;
+        var version = msie.exec(userAgent);
+        if (version<"MSIE 8") return false;
+        else return true;
+			}
+		}
+        var v = checkBrowser();
+		if (!v) {
+			window.location.href = "http://www.oppo.com/cn/lower.html";
+		}
+    </script>
+
+  <!--[if lt IE 9]>
+  <link media="all" type="text/css" rel="stylesheet" href="http://www.oppo.com/cn/assets/stylesheets/styles.css?v=201710091028">
+
+  <script src="https://shopfs.myoppo.com/cn/lib/html5shiv.js"></script>
+
+  <script src="https://shopfs.myoppo.com/cn/lib/respond.min.js"></script>
+
+  <script src="https://shopfs.myoppo.com/cn/lib/selectivizr-min.js"></script>
+
+  <link media="all" type="text/css" rel="stylesheet" href="https://shopfs.myoppo.com/cn/assets/stylesheets/ie.css?v=20161221100627">
+
+  <![endif]-->
+  <style type="text/css">
+    .mykq{background: #e96864;text-align: center; line-height: 30px;color: #fff;}
+    .mykq .wrapper{position: relative;}
+    .mykq p{font-size: 14px;padding-top: 5px;}
+    .mykq span{position: absolute;top:0;right: 0px;font-size: 25px; padding: 3px 40px;cursor: pointer;}
+    @media  screen and (max-width: 750px){
+        .mykq {text-align: left;line-height: 1.5}
+        .mykq p{padding: 8px 30px 8px 0;}
+        .mykq span{padding: 10px 20px;}
+    }
+</style>
+</head>
+
+<body>
+ <header class='main-header'>
+
+</header>
+
+  <main class="main-content">
+                <section class='full-carousel slick-dot-circle'>
+      <div class='hero slick-slide'>
+  <div class='hero-image center'>
+      <a   target='_blank' title='A77'>
+    <picture>
+      <!--[if IE 9]> <video style="display: none;"> <![endif]-->
+      <source media='(min-width: 990px)' srcset='https://dsfs.oppo.com/archives/201711/201711170911185a0e3b327f73b.jpg' />
+      <source media='(min-width: 750px)' srcset='https://dsfs.oppo.com/archives/201711/201711170911325a0e3b40144fd.jpg' />
+      <source media='(min-width: 300px)' srcset='https://dsfs.oppo.com/archives/201711/201711170911445a0e3b4c6b4e3.jpg' />
+      <!--[if IE 9]> </video> <![endif]-->
+      <img alt='R11sPlus' title='A77' src='https://dsfs.oppo.com/archives/201711/201711170911445a0e3b4c6b4e3.jpg' srcset='https://dsfs.oppo.com/archives/201711/201711170911185a0e3b327f73b.jpg'>
+    </picture>
+    </a>
+  </div>
+</div>
+  <div class='hero slick-slide'>
+  <div class='hero-image center'>
+      <a  target='_blank' title=''>
+    <picture>
+      <!--[if IE 9]> <video style="display: none;"> <![endif]-->
+      <source media='(min-width: 990px)' srcset='https://dsfs.oppo.com/archives/201711/201711161011445a0cf3a4028b6.jpg' />
+      <source media='(min-width: 750px)' srcset='https://dsfs.oppo.com/archives/201711/201711161011485a0cf3a8449a6.jpg' />
+      <source media='(min-width: 300px)' srcset='https://dsfs.oppo.com/archives/201711/201711161011515a0cf3ab9e6ab.jpg' />
+      <!--[if IE 9]> </video> <![endif]-->
+      <img alt='' title='' src='https://dsfs.oppo.com/archives/201711/201711161011515a0cf3ab9e6ab.jpg' srcset='https://dsfs.oppo.com/archives/201711/201711161011445a0cf3a4028b6.jpg'>
+    </picture>
+    </a>
+  </div>
+</div>
+  <div class='hero slick-slide'>
+  <div class='hero-image center'>
+      <a   target='_blank' title=''>
+    <picture>
+      <!--[if IE 9]> <video style="display: none;"> <![endif]-->
+      <source media='(min-width: 990px)' srcset='https://dsfs.oppo.com/archives/201711/201711160911035a0ce8b3869da.jpg' />
+      <source media='(min-width: 750px)' srcset='https://dsfs.oppo.com/archives/201711/201711160911075a0ce8b708141.jpg' />
+      <source media='(min-width: 300px)' srcset='https://dsfs.oppo.com/archives/201711/201711160911095a0ce8b944a16.jpg' />
+      <!--[if IE 9]> </video> <![endif]-->
+      <img alt='' title='' src='https://dsfs.oppo.com/archives/201711/201711160911095a0ce8b944a16.jpg' srcset='https://dsfs.oppo.com/archives/201711/201711160911035a0ce8b3869da.jpg'>
+    </picture>
+    </a>
+  </div>
+</div>
+  <div class='hero slick-slide'>
+  <div class='hero-image center'>
+      <a  target='_blank' title=''>
+    <picture>
+      <!--[if IE 9]> <video style="display: none;"> <![endif]-->
+      <source media='(min-width: 990px)' srcset='https://dsfs.oppo.com/archives/201711/201711161011475a0cf2f312add.jpg' />
+      <source media='(min-width: 750px)' srcset='https://dsfs.oppo.com/archives/201711/201711161011505a0cf2f6a792e.jpg' />
+      <source media='(min-width: 300px)' srcset='https://dsfs.oppo.com/archives/201711/201711161011545a0cf2fa1b5ab.jpg' />
+      <!--[if IE 9]> </video> <![endif]-->
+      <img alt='' title='' src='https://dsfs.oppo.com/archives/201711/201711161011545a0cf2fa1b5ab.jpg' srcset='https://dsfs.oppo.com/archives/201711/201711161011475a0cf2f312add.jpg'>
+    </picture>
+    </a>
+  </div>
+</div>
+    </section>
+    <section class="brick-feature">
+      <div class="g g-wrapper-s">
+          <div class="gi lap-one-third">
+        <div class='feature-product'>
+  <a target='_blank'>
+    <img alt='R11s & R11s Plus' title="R11s & R11s Plus" class='feature-product-image' src='https://dsfs.oppo.com/archives/201711/201711161011175a0cf2212430b.jpg'>
+    <div class='feature-product-content'>
+      <h2 class='feature-product-heading'></h2>
+      <p class='feature-product-description'>
+        <strong></strong>
+      </p>
+    </div>
+  </a>
+</div>
+      </div>
+          <div class="gi lap-one-third">
+        <div class='feature-product'>
+  <a  target='_blank'>
+    <img alt='A57' title="A57" class='feature-product-image' src='//static.oppo.com/archives/201709/2017091809095959bf1f0318037.jpg'>
+    <div class='feature-product-content'>
+      <h2 class='feature-product-heading'></h2>
+      <p class='feature-product-description'>
+        <strong></strong>
+      </p>
+    </div>
+  </a>
+</div>
+         </div>
+          <div class="gi lap-one-third">
+        <div class='feature-product'>
+  <a  target='_blank'>
+    <img alt='摄影廊' title="摄影廊" class='feature-product-image' src='https://dsfs.oppo.com/archives/201711/201711140911365a0a4dcc866c5.jpg'>
+    <div class='feature-product-content'>
+      <h2 class='feature-product-heading'></h2>
+      <p class='feature-product-description'>
+        <strong></strong>
+      </p>
+    </div>
+  </a>
+</div>
+</footer>
+
+<div class='navi-scroll'>
+  <div class="navi-scroll-content">
+    <a href="javascript:;" class="scroll-top-btn"><span>回到顶部</span><i></i></a>
+      </div>
+</div>
+
+<!-- <script src="http://www.oppo.com/cn/lib/jquery-1.9.1.min.js"></script>
+
+<script src="http://www.oppo.com/cn/lib/underscore-min.js"></script>
+
+<script src="http://www.oppo.com/cn/lib/slick.min.js"></script>
+
+<script src="http://www.oppo.com/cn/lib/responsive.js"></script>
+
+<script src="http://www.oppo.com/cn/lib/picturefill.min.js"></script>
+ -->
+<script src="https://shopfs.myoppo.com/cn/lib/lib.min.js"></script>
+
+<script src="https://shopfs.myoppo.com/cn/lib/custom_form.js?v=20150914144728"></script>
+
+<script src="https://shopfs.myoppo.com/cn/lib/scripts.js?v=201506151120596"></script>
+
+<script src="https://shopfs.myoppo.com/cn/assets/javascripts/oppo.min.js?v=201710091028"></script>
+
+
+
+
+
+
+</body>
+
+</html>
+<!-- cached at 2017-11-18 19:30:09 -->
